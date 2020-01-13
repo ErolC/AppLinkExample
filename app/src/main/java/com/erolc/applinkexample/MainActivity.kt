2330e3af3f12038ecdbf2a355e60d1f18710e735
Erolc <erolc@erolc.local>
2020-01-13 09:23:46 +0800

    完善

--- app/src/main/java/com/erolc/applinkexample/MainActivity.kt
+++ app/src/main/java/com/erolc/applinkexample/MainActivity.kt
@@ -3,6 +3,10 @@ package com.erolc.applinkexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.text).setOnClickListener {
-            share("https://github.com/ErolC/AppLinkExample/blob/master/example.html",filter = WECHAT)
+            share("https://erolc.github.io/example", filter = WECHAT)
         }
+
+        val i = Intent()
+        i.data = Uri.parse("https://erolc.github.io/applink")
+
+        Log.e("shixiangyu", i.toUri(Intent.URI_INTENT_SCHEME))
+
     }
 }
--- app/src/main/java/com/erolc/applinkexample/TargetActivity.kt
+++ app/src/main/java/com/erolc/applinkexample/TargetActivity.kt
@@ -2,6 +2,7 @@ package com.erolc.applinkexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class TargetActivity : AppCompatActivity() {
 
